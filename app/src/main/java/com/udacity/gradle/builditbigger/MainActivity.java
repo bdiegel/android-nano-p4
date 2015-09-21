3d37185227563ff3c420c1e6e984e46bdacf2232
Betty Diegel <bdiegel@gmail.com>
2015-09-21 16:18:06 -0400

    add async task to fetch joke from cloud endpoint
    - use 10.0.2.2 for stock emulator testing and *.3.2 for genymotion
    - limit GMS dependencties to play-services-ads
    - remove app dependency on java lib (now used by endpoint only)

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,24 +1,35 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.honu.haha.Joker;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.honu.punster.backend.jokeApi.JokeApi;
 import com.honu.standup.JokeActivity;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,12 +54,61 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        String joke = Joker.tellPun();
+        JokeAsyncTask asyncTask = new JokeAsyncTask();
+        asyncTask.execute();
+    }
+
+    private void showJokeActivity(String joke) {
         Intent jokeIntent = new Intent(this, com.honu.standup.JokeActivity.class);
         jokeIntent.putExtra(JokeActivity.EXTRA_JOKE_TEXT, joke);
         startActivity(jokeIntent);
-        //Toast.makeText(this, Joker.tellPun(), Toast.LENGTH_LONG).show();
     }
 
+
+    class JokeAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+
+        private JokeApi myApiService = null;
+
+        // localhost's IP address in Android emulator
+        //String rootUrl = "http://10.0.2.2:8080/_ah/api/";
+
+        // localhost's IP address in Genymotion emulator
+        String rootUrl = "http://10.0.3.2:8080/_ah/api/";
+
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+
+            // create the API service (do once)
+            if (myApiService == null) {
+                JokeApi.Builder builder = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(),
+                      new AndroidJsonFactory(), null)
+                      // local devappserver options:
+                      .setRootUrl(rootUrl)
+                      .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                          @Override
+                          public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                              // turn off compression
+                              abstractGoogleClientRequest.setDisableGZipContent(true);
+                          }
+                      });
+                      // end: local devappserver options
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.getPun().execute().getJoke();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(LOG_TAG, "Pun: " + result);
+            showJokeActivity(result);
+        }
+    }
 
 }
