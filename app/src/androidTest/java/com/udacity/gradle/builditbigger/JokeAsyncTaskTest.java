dc1c53817097cf6c72d91ea3d915fa6318294ab5
Betty Diegel <bdiegel@gmail.com>
2015-09-22 16:35:38 -0400

    create test to verifiy joke is fetched from service
    - created connected test using countdown latch to wait for result from async task
    - refactored async task to JokeFetcher wrapper class and create interface for callback
    - add progressbar to layout; display when fetching joke

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsyncTaskTest.java
@@ -0,0 +1,49 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.test.UiThreadTest;
+import android.text.TextUtils;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Connected test verifies that the a non-empty String is returned by JokeFetcher using an AsyncTask
+ */
+public class JokeAsyncTaskTest extends AndroidTestCase {
+
+    CountDownLatch signal;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        signal = new CountDownLatch(1);
+    }
+
+    @UiThreadTest
+    public void testFetchJoke() throws InterruptedException {
+        TestJokeListener listener = new TestJokeListener();
+        JokeFetcher jokeFetcher = new JokeFetcher(JokeFetcher.GENYMOTION_LOCALHOST, listener);
+        jokeFetcher.fetchJoke();
+        signal.await(15, TimeUnit.SECONDS);
+        //Log.d("JOKE", listener.joke);
+        assertTrue("Joke is non-empty", !TextUtils.isEmpty(listener.joke));
+    }
+
+    class TestJokeListener implements JokeFetcher.JokeListener {
+
+        public String joke = null;
+
+        @Override
+        public void onSuccess(String joke) {
+            this.joke = joke;
+            signal.countDown();
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            signal.countDown();
+        }
+    }
+}
